7bf96a1dd6f8f39203ab94f76025d0ed13471ffd
ian038 <ianphua3@gmail.com>
2022-01-27 18:27:45 -0500

    Added styling to sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,95 @@
+import { Avatar, IconButton, Button } from '@material-ui/core';
+import React from 'react';
+import styled from 'styled-components';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+export default function Sidebar() {
+    return (
+        <Container>
+            <Header>
+                <UserAvatar />
+                <IconsContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ExitToAppIcon />
+                    </IconButton>
+                </IconsContainer>
+            </Header>
+            <Search>
+                <SearchIcon />
+                <SearchInput placeholder="Search in Chat" />
+            </Search>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    flex: 0.45;
+    border-right: 1px solid whitesmoke;
+    height: 100vh;
+    min-width: 300px;
+    max-width: 350px;
+    overflow-y: scroll;
+    ::-webkit-scrollbar{
+        display: none;
+    }
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+`;
+
+const ChatLoading = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 30px 0px;
+`
+
+const Search = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+    outline: none;
+    border: none;
+    flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+    width: 100%;
+    &&& {
+        border-bottom: 1px solid whitesmoke;
+        border-top: 1px solid whitesmoke;
+    }
+`;
+
+const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    background-color: white;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+    :hover {
+        opacity: 0.8;
+    }
+`;
+
+const IconsContainer = styled.div``;
