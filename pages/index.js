c5db5f8c1af60a20b95a26c09ef46095280d52ae
ian038 <ianphua3@gmail.com>
2022-01-28 18:08:04 -0500

    Cleanup code

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Loading from '../components/Loading'
 import Sidebar from '../components/Sidebar'
 import styles from '../styles/Home.module.css'
 
