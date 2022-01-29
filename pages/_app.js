dede51b5d32d361fd45e30425f54ddcd3519dc79
ian038 <ianphua3@gmail.com>
2022-01-28 22:25:59 -0500

    Added database functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
 import '../styles/globals.css'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../firebase'
+import firebase from 'firebase/compat/app'
 import { useEffect } from 'react'
 import Login from './login'
 import Loading from '../components/Loading'
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth)
+
+  useEffect(() => {
+    if (user) {
+      db.collection('users').doc(user.uid).set({
+        email: user.email,
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+        photoURL: user.photoURL,
+        name: user.displayName
+      }, { merge: true })
+    }
+  }, [user])
 
   if (loading) return <Loading />
 
