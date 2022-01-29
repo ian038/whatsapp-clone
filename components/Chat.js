d893032ec56a456e632fe483ef9abb774ca5df53
ian038 <ianphua3@gmail.com>
2022-01-28 22:37:50 -0500

    Completed chat component

--- components/Chat.js
+++ components/Chat.js
@@ -6,6 +6,40 @@ import { db } from '../firebase';
 import getRecipientEmail from '../utils/getRecipientEmail';
 import { useRouter } from 'next/router'
 
-export default function Chat() {
-    return <div></div>;
+export default function Chat({ id, users, user }) {
+    const router = useRouter()
+    const recipientEmail = getRecipientEmail(users, user)
+    const [recipientSnapshot] = useCollection(db.collection('users').where('email', '==', getRecipientEmail(users, user)))
+    const recipient = recipientSnapshot?.docs?.[0]?.data()
+
+    const enterChat = () => {
+        router.push(`/chat/${id}`)
+    }
+
+    return (
+        <Container onClick={enterChat}>
+            {recipient ? (
+                <UserAvatar src={recipient?.photoURL} />
+            ) : (
+                <UserAvatar>{recipientEmail[0]}</UserAvatar>
+            )}
+            {recipient ? <p>{recipient?.name}</p> : <p>{recipientEmail}</p>}
+        </Container>
+    )
 }
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    padding: 15px;
+    word-wrap: break-word;
+    :hover{
+        background-color: whitesmoke;
+    }
+`
+
+const UserAvatar = styled(Avatar)`
+    margin: 5px;
+    margin-right: 15px;
+`
